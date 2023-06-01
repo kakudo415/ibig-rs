ff6588a111e5afa0e0058cf57cb5bfff44892f94
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-06-01 18:35:05 +0900

    feat: Add iBig, Add from_str, fmt::Display

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt;
+
+use anyhow::Result;
+
+#[allow(non_camel_case_types)]
+pub struct iBig {
+    digits: Vec<i8>,
+}
+
+impl iBig {
+    pub fn from_str(s: &str) -> Result<Self> {
+        let mut digits = Vec::new();
+        for c in s.chars() {
+            digits.push(c.to_digit(10).unwrap() as i8);
+        }
+        Ok(iBig { digits })
+    }
+}
+
+impl fmt::Display for iBig {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for digit in &self.digits {
+            write!(f, "{}", digit)?;
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +31,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn from_str_to_str() {
+        let hoge = iBig::from_str("1234567890").unwrap();
+        println!("iBig Display test: {}", hoge);      
     }
 }
+
