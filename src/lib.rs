b8a8dfbaf78e0e9405e2c83e0218bc3deec74b41
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-06-03 10:10:28 +0900

    test: Add random big test

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod mul;
 
 use std::fmt;
 
+use anyhow::Result;
+
 #[cfg(target_pointer_width = "64")]
 #[allow(non_camel_case_types)]
 type word = u64;
@@ -15,6 +17,20 @@ type dword = u128;
 #[derive(Debug, PartialEq, Eq)]
 pub struct uHuge {
     digits: Vec<word>,
+}
+
+impl uHuge {
+    pub fn from_str(s: &str) -> Result<Self> {
+        let mut digits = Vec::new();
+        let mut end = s.len();
+        let width = word::BITS as usize / 4;
+        while end > width {
+            digits.push(word::from_str_radix(&s[(end - width)..end], 16)?);
+            end -= width;
+        }
+        digits.push(word::from_str_radix(&s[..end], 16)?);
+        Ok(uHuge { digits })
+    }
 }
 
 pub(crate) fn pop_zero(digits: &mut Vec<word>) {
