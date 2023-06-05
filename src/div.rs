90d521d9956d0eba4a694ef68a56b66b788efe29
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-06-05 15:52:30 +0900

    fix: Adjust inverse precision

--- src/div.rs
+++ src/div.rs
@@ -14,25 +14,26 @@ impl ops::Div for &uHuge {
 }
 
 pub(crate) fn div(lhs: &[word], rhs: &[word]) -> Vec<word> {
-    let inverse = fixed_inverse(rhs);
+    let inverse = fixed_inverse(rhs, lhs.len() + 1);
     let mut acc = vec![0; lhs.len() + inverse.len()];
     mul_nn(&mut acc, lhs, &inverse);
     acc[inverse.len()..].to_vec() // remove digits after the point
 }
 
-// Calculate inverse of op in fixed point number by newton-raphson method
-pub(crate) fn fixed_inverse(op: &[word]) -> Vec<word> {
-    let mut inverse = vec![0; op.len() * 2];
-    let mut pred = vec![0; op.len() * 2];
-    let mut acc0 = vec![0; op.len() * 4];
-    let mut acc1 = vec![0; op.len() * 6];
+// Calculate inverse of op in fixed point number by Newton-Raphson method
+pub(crate) fn fixed_inverse(op: &[word], precision: usize) -> Vec<word> {
+    let len = op.len() + precision;
+    let mut inverse = vec![0; len];
+    let mut pred = vec![0; len];
+    let mut acc0 = vec![0; len * 2];
+    let mut acc1 = vec![0; len * 3];
 
     pred[op.len()] = 1; // initial predict
     'check_accuracy: loop {
-        mul_nn(&mut acc0[..op.len() * 4], &pred, &pred);
+        mul_nn(&mut acc0[..len * 2], &pred, &pred);
         mul_assign_n1(&mut pred, 2);
-        mul_nn(&mut acc1[..op.len() * 6], &acc0[..op.len() * 4], op);
-        sub_assign(&mut pred, &acc1[op.len() * 2..op.len() * 4]);
+        mul_nn(&mut acc1[..len * 3], &acc0[..len * 2], op);
+        sub_assign(&mut pred, &acc1[len..len * 2]);
 
         for i in 0..inverse.len() {
             if inverse[i] != pred[i] {
@@ -56,7 +57,7 @@ mod tests {
         let ans = uHuge {
             digits: vec![0, 1 << (word::BITS - 1)],
         };
-        assert_eq!(fixed_inverse(&operand.digits), ans.digits);
+        assert_eq!(fixed_inverse(&operand.digits, 1), ans.digits);
     }
 
     #[test]
@@ -72,6 +73,14 @@ mod tests {
         let lhs = uHuge::from_str("1234567890").unwrap();
         let rhs = uHuge::from_str("ABCDE").unwrap();
         let ans = uHuge::from_str("1B203").unwrap();
+        assert_eq!(&lhs / &rhs, ans);
+    }
+
+    #[test]
+    fn div_random_0() {
+        let lhs = uHuge::from_str("123285196341321511").unwrap();
+        let rhs = uHuge::from_str("2561").unwrap();
+        let ans = uHuge::from_str("7CA130980b6012").unwrap();
         assert_eq!(&lhs / &rhs, ans);
     }
 }
